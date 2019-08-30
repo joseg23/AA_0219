42aef413be75cb79ddbb8a328f1d86bb41f190aa
JoseG23 <JoseG23@LAPTOP-O96HFMJ2>
2019-08-30 17:18:55 -0600

    Empezando tarea del labo 2.

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: JoseG23
+ *
+ * Created on 30 de agosto de 2019, 05:08 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
