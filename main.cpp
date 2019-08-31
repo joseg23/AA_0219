e97be4fa1e834e2285f5f862bb16c17ba1d58bc7
JoseG23 <JoseG23@LAPTOP-O96HFMJ2>
2019-08-30 20:05:22 -0600

    "Terminado"

--- main.cpp
+++ main.cpp
@@ -1,25 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
  * Author: JoseG23
- * 00051716
+ * Carnet: 00051716
  * Created on 30 de agosto de 2019, 05:08 PM
  */
 
-
 #include <iostream>
-#include <string>
+
 using namespace std;
+
+//Si no te corre en Neatbeans lo podes probar en C++ Shell.
 
 struct Node{
     int data;
-    Nodo *next;
-}*begin;
+    Node *next;
+}*star;
 
 class Stack{
 public:
@@ -29,33 +24,63 @@ public:
         n->next= nullptr;
         return n;
     }
-    
-    void push(int value){
-        Node *n=crearNodo(value);
-        n->next=begin;
-        begin=n;
-    }
-    
+         
     void pop(){
-        Nodo *temp=begin;
-        begin=begin->next;
+        Node *temp=star;
+        star=star->next;
         free(temp);
     }
     
+    void push(int value){
+        Node *n=createNode(value);
+        n->next=star;
+        star=n;
+    }
+
     void showStack(){
-        Nodo *temp=begin;
-        if(!begin)
-            cout<<"Empty Stack" << endl;
+        Node *temp=star;
+        if(!star)
+            cout<<"Empty Stack"<<endl;
         else
             while (temp) {
-                cout<< temp->data << ",";
+                cout<< temp->data<<" ";
                 temp=temp->next;
             }
     }
-    
-    Stack(){
-        begin=nullptr;
-    }
 };
 
+void array(){
+    int i,odd=0,even=0;
+    int A[]={122,20,83,14,55,68}; //IMGRESAR DATOS A SU ANTOJO
+    int size=*(&A + 1) - A;
+    Stack stack;
+    
+    for(i=0; i<size; i++){
+        if(A[i]%2==0)
+        {
+            even++;
+            stack.push(A[i]);
+        }
+        else{
+            odd++;
+            stack.push(A[i]);
+        }
+    }
+    
+    cout<<"Pila: "<<endl;
+    stack.showStack();
+    
+    cout<<"\n\nTotal pares: "<<even<<endl;
+    cout<<"Total impares: "<<odd<<endl;
+    
+    if(even==odd)
+        cout<<"\nSon pilas iguales"<<endl;
+    else
+        cout<<"\nSon pilas distintas"<<endl;
+}
+ 
+int main(){
+    array();
+    return 0;
+}
 
