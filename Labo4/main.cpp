36a514dc10f7b87755960a4c5e3115495fc1bb6c
JoseG23 <JoseG23@LAPTOP-O96HFMJ2>
2019-09-18 21:56:08 -0600

    Agregando funcion de intercambio de elementos

---
+++ Labo4/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: JoseG23
+ *
+ * Created on 18 de septiembre de 2019, 09:53 PM
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int*x, int*y)
+{
+    int z=*x;
+    *x=*y;
+    *y=z;
+}
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
